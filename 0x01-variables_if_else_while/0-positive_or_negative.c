04f91a5cb87773ef81a1ccf46011850861db35c4
Sani Bilyaminu <sani.bilyaminn@gmail.com>
2022-12-11 14:49:34 -0800

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,16 +15,10 @@ int main(void)
 	n = rand() - RAND_MAX;
 
 	if (n > 0)
-	{
 		printf("d% is positive\n", n);
-	}
 	else if (n == 0)
-	{
 		printf("d% is zero\n", n);
-	}
-	else if (n < 0)
-	{
+	else
 		printf("d% is negative\n", n);
-	}
 	return (0);
 }
