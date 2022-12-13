04a1949c22786c121c8aaf49fae231288c373487
Sani Bilyaminu <sani.bilyaminn@gmail.com>
2022-12-13 04:02:34 -0800

    Advanced Task 3

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,16 +12,15 @@ int main(void)
 
 	for (a = 0; a < 8; a++)
 	{
-
 		for (b = a + 1; b < 9; b++)
 		{
 			for (c = b + 1; c < 10; c++)
 			{
 				putchar((a % 10) + '0');
-				putchar((a % 10) + '0');
-				putchar((a % 10) + '0');
+				putchar((b % 10) + '0');
+				putchar((c % 10) + '0');
 
-				if (a ==8 && b == 9 && c == 10)
+				if (a == 8 && b == 9 && c == 10)
 					continue;
 
 				putchar(',');
